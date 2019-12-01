30f2666ffdd2ebb32864caa356e9d7d812c5323a
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-01 20:07:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,12 +13,14 @@ const codes = [
 
 let index = 0;
 var keyLogger=[];
+
 function onKeyDownHandler(e){
   const key = e.key;
   console.log(`This is index before if: ${index}`)
   console.log(`This is eKey: ${e.key}`)
   console.log(`This is key: ${key}`)
   keyLogger.push(e.key);
+  console.log(keyLogger)
   if (key === codes[index]) {
     index++;
     console.log(`This is index after addition: ${index}`)
