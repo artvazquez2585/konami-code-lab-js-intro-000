9ae749a48b763a2040ba56b22337de94edfbb8b0
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-12-01 19:15:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,18 @@ const codes = [
   "a"
 ];
 
+var keyLogger
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown',
+  function(e){
+    keyLogger.push(e.key)
+    if (keyLogger.length > codes.length){
+      keyLogger.shift();
+    }
+    if (array == codes){
+      alert('All power-ups are active!')
+    }
+  })
 }
